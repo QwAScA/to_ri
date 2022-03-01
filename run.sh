f06121f321873a2e88cd52f2c29b922c0223dbe2
Pavel Novikov <p.novikov@itransition.com>
2022-03-01 21:27:33 +0200

    Fix typo

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ bash -c "./update_target.sh"
 
 for TARGET in $(head -n 1 ru.txt); do
   sed -i 's/IP_TARGET/'${TARGET}'/g' .github/workflows/action.yml
-  git checkout -n target_${TARGET}
+  git checkout -b target_${TARGET}
   git add .; git commit -m "add"
   git push --set-upstream origin target_${TARGET}
   git checkout main; git pull
